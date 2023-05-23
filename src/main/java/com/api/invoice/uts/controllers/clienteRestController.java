230a3fc9030f65773219eaefe8b1dfc0a5b8c6cf
montanez8 <montanezcompany.r@gmail.com>
2023-05-22 19:31:59 -0500

    v1

--- src/main/java/com/api/invoice/uts/controllers/clienteRestController.java
+++ src/main/java/com/api/invoice/uts/controllers/clienteRestController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = { "http://localhost:4200"})
+// @CrossOrigin(origins = { "http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class clienteRestController {
